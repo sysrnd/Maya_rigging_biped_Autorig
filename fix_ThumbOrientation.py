3b15a8a75ca2fdc17a5f8820b21d97e03e0a7e5c
arturoalcibia <arturoalcibia@hotmail.com>
2018-02-10 15:49:26 -0600

    fixed thumb

--- fix_ThumbOrientation.py
+++ fix_ThumbOrientation.py
@@ -7,29 +7,17 @@ JNT_IZQ = 'THUMB_IZQ_'
 AXIS = ['X']
 
 JNTHAND = 'MANO_DER'
-for num in xrange(2, 4):
-	num = str(num)
-	inconmingCons = cmds.listConnections(CTRL_DER + num, scn=True, t='orientConstraint')[0]
-	cmds.delete(inconmingCons)
 
-for num in xrange(2, 4):
-	num = str(num)
-	cmds.parent('GRP_' + CTRL_DER + num, JNTHAND)
 
-for num in xrange(2, 4):
+cmds.delete(cmds.listRelatives(CTRL_DER + '2', parent=True)[0])
 
-	if num == 2:
-		resta = 160
-	else:
-		resta = 90
-
-	num = str(num)
-	srcJnt = 'GRP_' + CTRL_IZQ + num
-	destJnt = 'GRP_' + CTRL_DER + num
-	attr = cmds.getAttr(srcJnt + '.rotateX')
-	attr -= resta
-	cmds.setAttr(destJnt + '.rotateX', attr)
-
+parent = cmds.listRelatives(CTRL_IZQ + '2', p=True)[0]
+dup = cmds.duplicate(parent, n=CTRL_IZQ + 'GRP_2')
+dup = cmds.parent(dup[0], w=True)
+newGrp = cmds.group(dup[0], n='tempGrp')
+cmds.xform(newGrp, ws=True, piv=(0, 0, 0))
+cmds.setAttr(newGrp + '.scaleX', -1)
+cmds.parent(cmds.listRelatives(newGrp, c=True), CTRL_DER + '1')
 
 for num in xrange(2, 4):
 	num = str(num)
@@ -39,3 +27,8 @@ for num in xrange(2, 4):
 for num in xrange(2, 4):
 	num = str(num)
 	cmds.orientConstraint(CTRL_DER + num, JNT_DER + num, mo=True)
+
+def replaceNameRecursively(name):
+	newName = name.replace('IZQ', 'DER')
+	cmds.rename(name, newName)
+	child = cmds.listRelatives(newName, c=True)
